7cbfb29420a97bafa8416f6d5da44d2863f4f11b
netboy01 <30308840+netboy01@users.noreply.github.com>
2020-04-15 20:15:45 +0530

    Create out_script.sh

---
+++ out_script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#####################################################
+#Script Name 	: out_script.sh 
+#Description 	: script to build the docker image and run the docker image.
+#Input Argument : MAC address in parameter file.
+#Name 		: Asif Iqbal
+#Date		: 14th April 2020
+########################################################
+
+echo "Building Docker image Started"
+
+docker build -t demo_project . > /dev/null 2>&1
+
+res=$?
+
+if [ $res != '0' ]
+then
+	echo " Docker image build Failed"
+ 	exit 1; 	
+fi
+sleep 1
+echo "Docker Image build sucesssfully"
+
+
+PARM1=$(cat parameter_file.txt)
+
+#Command to run the docker image
+
+docker run --name demo_name_$RANDOM  --env MAC_ADD=$PARM1 demo_project
